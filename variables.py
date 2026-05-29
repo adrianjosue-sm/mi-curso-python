77476f5beb6f0628f9aa41717078801e15817769
Adrian_Medina <adrianjosue.sm@gmail.com>
2026-05-28 21:12:41 -0600

    Aprendiendo sobre variables y cajas de juguetes

---
+++ variables.py
@@ -0,0 +1,17 @@
+# Aqui creamos las cajitas (Variables)
+nombre_jugador = "Cientifico Linux"  
+nivel = 1  
+precio_moneda = 4.50
+tiene_superpoderes = True
+
+# Ahora le pedimos a Python que nos muestre que hay dentro de las cajitas
+print("--- DATOS DEL JUGADOR ---")
+print("Nombre: ", nombre_jugador)
+print("Nivel actual: ", nivel)
+print("Precio de su moneda: ", precio_moneda)
+print("¿Tiene supepoderes?: ", tiene_superpoderes)
+
+# ¡Magia! Vamos a cambiar lo que hay dentro de una caja (por eso se llaman variables)
+nivel = nivel + 1
+print("Felicidades! Subiste de nivel. Ahora tu nivel es: ", nivel)
+
