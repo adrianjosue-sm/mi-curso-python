ce556f0040e05ac3b1e13c33d6e1b0e0b4c2f89b
Adrian_Medina <adrianjosue.sm@gmail.com>
2026-06-10 21:32:31 -0600

    Aprendiendo estructuras de datos: Listas y Diccionarios

---
+++ organizadores.py
@@ -0,0 +1,34 @@
+# Vamos a crear un sistema que maneje una lista de juegos más vendidos y un diccionario con los detalles de una consola.
+
+print("---EJEMPLO 1: NUESTRA LISTA DE VIDEOJUEGOS---")
+
+# Creamos una lista usando corchetes [ ]
+juegos = ["Minecraft", "Zelda", "Mario Kart"]
+
+# Ver la lista completa
+print("Lista completa de videojuegos: ", juegos)
+
+# Ver solo el primer videojuego.
+print("El juego en la posicion 1 es: ", juegos[0])
+
+# Agregamos un videojuego mas al final de la lista.
+juegos.append("Sonic")
+print("Lista actualizada: ", juegos)
+
+
+print("\n ---EJEMPLO 2: EL DICCIONARIO DE LA CONSOLA---")
+
+# Creamos un diccionario usando llaves { }
+consola = {
+    "nombre": "Play Station",
+    "color": "Negro y plateado",
+    "almacenamiento_GB": 64,
+    "nueva": True
+}
+
+# Ver tola la informacion de la consola.
+print("Ficha tecnica de la consola:", consola)
+
+# Buscar un dato específico usando su clave entre corchetes
+print("¿De qué color es?: ", consola["color"])
+print("¿Cuánta memoria tiene?: ", consola["almacenamiento_GB"], "GB")
